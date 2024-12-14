391007757e53ff78480ec3911ed2876339cfcf12
Fabian Jimenez-Curiel <jimenfab@oregonstate.edu>
2024-12-14 10:03:20 -0800

    updated edit functions for pilots, default values are used if no changes are made to a value inside the edit page.

--- Frontend/src/pages/updatePilot.jsx
+++ Frontend/src/pages/updatePilot.jsx
@@ -4,9 +4,9 @@ import { useNavigate, useParams } from 'react-router-dom';
 
 function updatePilot() {
     const [data, setData] = useState([])
-    const [fname, setFname] = useState('')
-    const [lname, setLname] = useState('')
-    const [certs, setCerts] = useState('')
+    let [fname, setFname] = useState('')
+    let [lname, setLname] = useState('')
+    let [certs, setCerts] = useState('')
 
     const navigate = useNavigate();
     const {id} = useParams();
@@ -15,12 +15,23 @@ function updatePilot() {
         axios.get(`http://localhost:8081/getPilot/${id}`)
         .then((res) => {
             setData(res.data);
+            console.log(res.data)
         })
         .catch((err) => console.log(err));
     }, [id]);
 
     function handleSubmit(event) {
         event.preventDefault();
+        //use default values if no change was made
+        if(fname === ""){
+            fname = data[0].fname;
+        }
+        if(lname === ""){
+            lname = data[0].lname;
+        }
+        if(certs === ""){
+            certs = data[0].totalCertificate;
+        }
         axios.put(`http://localhost:8081/updatePilot/${id}`, {fname, lname, certs})
         .then(res => {
             navigate('/pilots');
@@ -38,18 +49,18 @@ function updatePilot() {
                     <h2>Edit Pilot Info: {pilots.fname} {pilots.lname}</h2>
                     <div className='mb-2'>
                         <label htmlFor="fname">First Name</label>
-                        <input Value={pilots.fname} key={pilots.fname} type='text' name='fname' 
+                        <input defaultValue={pilots.fname} key={pilots.fname} type='text' name='fname' 
                         autoFocus className='form-control' required 
                         onChange={(e) => setFname(e.target.value)} />
                     </div>
                     <div className='mb-2'>
                         <label htmlFor="lname">Last Name</label>
-                        <input Value={pilots.lname} type='text' name='lname' className='form-control' required 
+                        <input defaultValue={pilots.lname} type='text' name='lname' className='form-control' required 
                         onChange={(e) => setLname(e.target.value)} />
                     </div>
                     <div className='mb-2'>
                         <label htmlFor="certs">Total Certificates</label>
-                        <input Value={pilots.totalCertificate} type='number' name='certs' className='form-control' required 
+                        <input defaultValue={pilots.totalCertificate} type='number' name='certs' className='form-control' required 
                         onChange={(e) => setCerts(e.target.value)} />
                     </div>
                     <button type="submit" className='btn btn-success'>Update</button>
