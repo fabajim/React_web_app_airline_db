ddbcea41c29fa7a4772fe151c9da9ca96711dbce
Fabian Jimenez-Curiel <jimenfab@oregonstate.edu>
2025-01-01 13:43:02 -0800

    Assignment details page now renders the assignmentdetails table.

--- Frontend/src/pages/assignmentDetails.jsx
+++ Frontend/src/pages/assignmentDetails.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+//import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function assignmentDetails() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8081/assignmentDetails')
+        .then((res)=> {
+            setData(res.data);
+            console.log(res.data);
+        })
+        .catch((err)=> console.log(err));
+    }, []);
+
   return (
-    <div>assignmentDetails</div>
+    <>
+    <h1 className='page-name'>Assignments Log</h1>
+      <table className="read-table">
+        <thead>
+          <tr>
+            <th>Assignment ID</th>
+            <th>Pilot</th>
+            <th>Aircraft</th>
+            <th>Current Location</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((d, i) => {
+            return(
+              <tr key = {i}>
+                <td>{d.assignmentDetailID}</td>
+                <td>{d.fname} {d. lname}</td>
+                <td>{d.serialNum}</td>
+                <td>{d.cityCode}</td>
+              </tr>)
+          })}
+        </tbody>
+      </table>
+    </>
   )
 }
 
