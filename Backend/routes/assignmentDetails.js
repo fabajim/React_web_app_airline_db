9ab6b1be85e6cfdf7e0b9c5304e3cd8a2548be3b
Fabian Jimenez-Curiel <jimenfab@oregonstate.edu>
2025-01-01 12:44:15 -0800

    added inner join on airports in the get api for assignmentDetails.js.

--- Backend/routes/assignmentDetails.js
+++ Backend/routes/assignmentDetails.js
@@ -4,10 +4,12 @@ const db = require('../database/db_connector.js');
 
 router.get('/', (req, res) => {
     const getQuery = `SELECT assignmentDetailID, Pilots.pilotID, Pilots.fname, Pilots.lname, 
-                        Aircraft.aircraftID, Aircraft.serialNum
+                        Aircraft.aircraftID, Aircraft.serialNum,
+                        Airports.airportID, Airports.cityCode
 	                    FROM AssignmentDetails
                         INNER JOIN Pilots ON AssignmentDetails.pilotID = Pilots.pilotID
-                        INNER JOIN Aircraft ON AssignmentDetails.aircraftID = Aircraft.aircraftID`;
+                        INNER JOIN Aircraft ON AssignmentDetails.aircraftID = Aircraft.aircraftID
+                        INNER JOIN Airports ON AssignmentDetails.airportID = Airports.airportID`;
     db.pool.query(getQuery, (err, data) => {
         if (err) return res.json(err);
         return res.json(data);
