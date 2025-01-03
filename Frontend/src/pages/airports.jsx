482fdd7dfbf2e370278ac5c2868124b3d23a6102
Fabian Jimenez-Curiel <jimenfab@oregonstate.edu>
2025-01-03 15:28:44 -0800

    Airports hub status can now be updated with the click of the update button.

--- Frontend/src/pages/airports.jsx
+++ Frontend/src/pages/airports.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import Swal from 'sweetalert2'
 
 function airports() {
     const [data, setData] = useState([]);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get('http://localhost:8081/airports')
@@ -13,6 +16,34 @@ function airports() {
         })
         .catch((err)=> console.log(err));
     }, []);
+
+    function update(id, hub, code) {
+      Swal.fire({
+        title: `Update Hub status?`,
+        text: `${code} will have its hub updated.`,
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d33",
+        cancelButtonColor: "#3085d6",
+        confirmButtonText: "Change"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          handleUpdate(id, hub);
+        }
+      });
+    }
+
+    function handleUpdate(id, hub) {
+      let updateHub =  hub === 'YES' ? 0 : 1;
+      axios.put(`http://localhost:8081/airports/${id}`, {updateHub})
+      .then((res) => {
+        alert('Airport Updated');
+        navigate(0);
+      })
+      .catch((err) => {
+        alert('ERROR ' + err.status);
+      })
+    }
 
   return (
     <>
@@ -42,7 +73,13 @@ function airports() {
                 <td>{d.cityCode}</td>
                 <td>{d.isHub}</td>
                 <td className='total-aircraft'>{d.totalAircraft}</td>
-                <td></td>
+                <td>
+                  <button
+                    className='btn btn-secondary btn-sm'
+                    onClick={ () => update(d.airportID, d.isHub, d.cityCode) }>
+                  Update    
+                  </button>
+                </td>
               </tr>)
           })}
         </tbody>
