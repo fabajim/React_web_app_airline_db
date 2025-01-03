8048bc27ec12ffbd9e0fd54e6e5964136b86bba1
Fabian Jimenez-Curiel <jimenfab@oregonstate.edu>
2025-01-03 11:30:03 -0800

    updated the get query in airports to get a count of aircraft in the airport from the assinmentDetails page.

--- Backend/routes/airports.js
+++ Backend/routes/airports.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const db = require('../database/db_connector.js');
 
 router.get('/', (req, res) => {
-    const getQuery = `SELECT airportID, city, cityCode, totalAircraft,
-                      IF(isHub = 1, 'YES', 'NO') AS isHub
-                      FROM Airports`;
+    const getQuery = `SELECT Airports.airportID, Airports.cityCode, Airports.city, 
+                            IF(Airports.isHub = 1, 'YES', 'NO') AS isHub,
+	                        COUNT(assignmentDetails.airportID) AS totalAircraft
+                        FROM Airports
+                        LEFT JOIN assignmentDetails ON Airports.airportID = assignmentDetails.airportID
+                        AND assignmentDetails.isActive = 1
+                        GROUP BY Airports.airportID`;
     db.pool.query(getQuery, (err, data) => {
         if (err) {
             console.log(err);
